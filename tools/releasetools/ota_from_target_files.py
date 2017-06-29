3a62256c3e8e8bcc4005317d4bffc80acbf482c6
Tab Fitts <tfitts1212@gmail.com>
2017-06-29 16:28:08 -0700

    build/release-tools: Fusionify edify code.
    Change-Id: I3755187d5bfdcabc6b108d7e27e30b9491f751f3

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -698,9 +698,12 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   #script.Print("Target: %s" % CalculateFingerprint(
   #    oem_props, oem_dict, OPTIONS.info_dict))
 ##script.Print("**************************************************");
-  script.Print("********************")
-  script.Print("**   Pure Nexus   **")
-  script.Print("********************")
+  script.Print("***********************************")
+  script.Print("***********************************")
+  script.Print("**********Pure Fusion OS***********")
+  script.Print("***********************************")                                                                
+  script.Print("***********************************")
+  script.Print("***********************************")
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()

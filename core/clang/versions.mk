4b227a9e9da5c22237280b38449246427d4944ae
Eliminater74 <eliminater74@gmail.com>
2017-09-30 06:44:36 -0400

    [build] [clang] V6.x Full Blown, Now thats playing with Power:
    Change-Id: I89a5850163caa8a61d1c9d661a7b27132a660bd2
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- core/clang/versions.mk
+++ core/clang/versions.mk
@@ -1,8 +1,8 @@
 ## Clang/LLVM release versions.
-LLVM_RELEASE_VERSION := 5.0
-LLVM_PREBUILTS_VERSION := 5.0
+LLVM_RELEASE_VERSION := 6.0
+LLVM_PREBUILTS_VERSION := 6.0
 LLVM_PREBUILTS_BASE ?= prebuilts/clang/host
 
 # RenderScript-specific tools
-RS_LLVM_PREBUILTS_VERSION := 3.9
+RS_LLVM_PREBUILTS_VERSION := 6.0
 RS_LLVM_PREBUILTS_BASE := prebuilts/clang/host
